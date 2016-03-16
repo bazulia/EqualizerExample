1772a1082e8f94f040932e98e6fbb65f17316f7d
Igor Bezeka <bazulia08@gmail.com>
2016-03-16 23:51:35 +0200

    Initial commit 3 lab Software support

---
+++ app/src/main/java/com/example/bezeka/equalizerexample/MainActivity.java
@@ -0,0 +1,138 @@
+package com.example.bezeka.equalizerexample;
+
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.media.audiofx.Equalizer;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "AudioFxDemo";
+
+    private MediaPlayer mMediaPlayer;
+    private Equalizer mEqualizer;
+
+    private LinearLayout mLinearLayout;
+    private TextView mStatusTextView;
+
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+
+        setVolumeControlStream(AudioManager.STREAM_MUSIC);
+
+        mStatusTextView = new TextView(this);
+
+        mLinearLayout = new LinearLayout(this);
+        mLinearLayout.setOrientation(LinearLayout.VERTICAL);
+        mLinearLayout.addView(mStatusTextView);
+
+        setContentView(mLinearLayout);
+
+        // Create the MediaPlayer
+        mMediaPlayer = MediaPlayer.create(this, R.raw.recording);
+        Log.d(TAG, "MediaPlayer audio session ID: " + mMediaPlayer.getAudioSessionId());
+
+        setupEqualizerFxAndUI();
+
+        // When the stream ends, we don't need to collect any more data. We don't do this in
+        // setupVisualizerFxAndUI because we likely want to have more, non-Visualizer related code
+        // in this callback.
+        mMediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            public void onCompletion(MediaPlayer mediaPlayer) {
+            }
+        });
+
+        mMediaPlayer.start();
+        mStatusTextView.setText("Playing audio...");
+    }
+
+    private void setupEqualizerFxAndUI() {
+        // Create the Equalizer object (an AudioEffect subclass) and attach it to our media player,
+        // with a default priority (0).
+        mEqualizer = new Equalizer(0, mMediaPlayer.getAudioSessionId());
+        mEqualizer.setEnabled(true);
+
+        TextView eqTextView = new TextView(this);
+        eqTextView.setText("Equalizer:");
+        mLinearLayout.addView(eqTextView);
+
+        short bands = mEqualizer.getNumberOfBands();
+
+        final short minEQLevel = mEqualizer.getBandLevelRange()[0];
+        final short maxEQLevel = mEqualizer.getBandLevelRange()[1];
+
+        for (short i = 0; i < bands; i++) {
+            final short band = i;
+
+            TextView freqTextView = new TextView(this);
+            freqTextView.setLayoutParams(new ViewGroup.LayoutParams(
+                    ViewGroup.LayoutParams.FILL_PARENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT));
+            freqTextView.setGravity(Gravity.CENTER_HORIZONTAL);
+            freqTextView.setText((mEqualizer.getCenterFreq(band) / 1000) + " Hz");
+            mLinearLayout.addView(freqTextView);
+
+            LinearLayout row = new LinearLayout(this);
+            row.setOrientation(LinearLayout.HORIZONTAL);
+
+            TextView minDbTextView = new TextView(this);
+            minDbTextView.setLayoutParams(new ViewGroup.LayoutParams(
+                    ViewGroup.LayoutParams.WRAP_CONTENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT));
+            minDbTextView.setText((minEQLevel / 100) + " dB");
+
+            TextView maxDbTextView = new TextView(this);
+            maxDbTextView.setLayoutParams(new ViewGroup.LayoutParams(
+                    ViewGroup.LayoutParams.WRAP_CONTENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT));
+            maxDbTextView.setText((maxEQLevel / 100) + " dB");
+
+            LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
+                    ViewGroup.LayoutParams.FILL_PARENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT);
+            layoutParams.weight = 1;
+            SeekBar bar = new SeekBar(this);
+            bar.setLayoutParams(layoutParams);
+            bar.setMax(maxEQLevel - minEQLevel);
+            bar.setProgress(mEqualizer.getBandLevel(band));
+            mEqualizer.setBandLevel(band, (short) (1500));
+            bar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+                public void onProgressChanged(SeekBar seekBar, int progress,
+                                              boolean fromUser) {
+                    mEqualizer.setBandLevel(band, (short) (progress + minEQLevel));
+                    Log.d("MyTAG","Min: " + minEQLevel + ", progress: " + progress + " , max: " + maxEQLevel);
+                    Log.d("MyTAG", "Curr: " + mEqualizer.getBandLevel(band) + " , progress + min: " + (progress + minEQLevel));
+                }
+
+                public void onStartTrackingTouch(SeekBar seekBar) {}
+                public void onStopTrackingTouch(SeekBar seekBar) {}
+            });
+
+            row.addView(minDbTextView);
+            row.addView(bar);
+            row.addView(maxDbTextView);
+
+            mLinearLayout.addView(row);
+        }
+    }
+
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        if (isFinishing() && mMediaPlayer != null) {
+            mEqualizer.release();
+            mMediaPlayer.release();
+            mMediaPlayer = null;
+        }
+    }
+}
